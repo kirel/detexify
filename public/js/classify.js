25bf2d4175bc7ce806d72a18e30dac270dae68f9
Daniel Kirsch <danishkirel@gmail.com>
2009-06-29 09:33:55 +0200

    changed navigation

--- public/js/classify.js
+++ public/js/classify.js
@@ -7,13 +7,13 @@ $(function(){
   function classify(canvas) {
     abort = false;
     var url = canvas.toDataURL();
-    $('#spinner').show();
+    $('#spinner').show('scale');
     $.post("/classify", { "url": url, "strokes": JSON.stringify(canvas.strokes) }, function(json) {
       if (!abort) {
+        $('#spinner').hide('scale');        
         $('#hitlist').empty();
         //$('#classinatra').text('Es wurde '+json.url+' angefordert.');
         jQuery.each( json.hits, function() {
-          $('#spinner').hide();        
           $('#hitlist').append('<tr><td><code>'+this.tex+'</code></td><td class="symbol"><img alt="tex:'+this.tex+'"/></td><td class="score">'+this.score+'</td></tr>').show();
         });
         mathtex.init();
@@ -26,7 +26,7 @@ $(function(){
   var c = $("#tafel").get(0);
   $('#clear').click(function(){
     abort = true;
-    clearCanvas(c);
+    c.clear();
     $('#hitarea').hide();
     $('#hitlist').empty();
     $('#spinner').hide();    
