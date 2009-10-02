436751a546a3c750affa4b3cbe8a2e13e1d3796a
Daniel Kirsch <danishkirel@gmail.com>
2009-10-02 21:56:48 +0200

    more classifiers

--- my_classifiers.rb
+++ my_classifiers.rb
@@ -8,7 +8,15 @@ classifier :default do |cache|
   KnnClassifier.new(Detexify::Extractors::Strokes::Features.new, lambda { |v,w| (v-w).r }, :cache => cache)
 end
 
-classifier :elastic do
+classifier :largek do |cache|
+  KnnClassifier.new(Detexify::Extractors::Strokes::Features.new, lambda { |v,w| (v-w).r }, :k => 25, :cache => cache)
+end
+
+classifier :ten do |cache|
+  KnnClassifier.new(Detexify::Extractors::Strokes::Features.new, lambda { |v,w| (v-w).r }, :k => 10, :limit => 10, :cache => cache)
+end
+
+classifier :dcelastic do
   Classifiers::DCPruningKnnClassifier.new(
     Detexify::Preprocessors::Pipe.new(
       Detexify::Preprocessors::Strokes::SizeNormalizer.new,
@@ -16,4 +24,15 @@ classifier :elastic do
     ),
     MultiElasticMatcher,
     [lambda { |i| i.size }, Detexify::Extractors::Strokes::AspectRatio.new(4)])
+end
+
+classifier :tenelastic do
+  Classifiers::KnnClassifier.new(
+    Detexify::Preprocessors::Pipe.new(
+      Detexify::Preprocessors::Strokes::SizeNormalizer.new,
+      Detexify::Preprocessors::Strokes::EquidistantPoints.new(:distance => 0.2)
+    ),
+    MultiElasticMatcher, # measure
+    :limit => 10
+  )
 end
