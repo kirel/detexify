b54b88b86e07626b177d1f682dc800ed1951a2eb
Daniel Kirsch <danishkirel@gmail.com>
2010-06-28 12:35:18 +0200

    current bundler

--- init.rb
+++ init.rb
@@ -1,12 +1,6 @@
-begin
-  # Require the preresolved locked set of gems.
-  require ::File.expand_path('../.bundle/environment', __FILE__)
-rescue LoadError
-  # Fallback on doing the resolve at runtime.
-  require "rubygems"
-  require "bundler"
-  Bundler.setup
-end
+require "rubygems"
+require "bundler"
+Bundler.setup
 
 $LOAD_PATH << ::File.join(::File.dirname(__FILE__), 'lib')
 
